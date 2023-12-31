ceb8cfcd685c5ce038579b8781c83a6dcfed498d
Appsgalore33 <richardchance1980@gmail.com>
2023-12-31 13:07:53 -0600

    Removed code from App.js

--- App.js
+++ App.js
@@ -4,9 +4,7 @@ import CounterView from './src/views/CounterView';
 
 const App = () => (
   <SafeAreaView style={{ flex: 1 }}>
-    <Text style={{ textAlign: 'center', margin: 10 }}>Counter App</Text>
     <CounterView />
-    <Text style={{ textAlign: 'center', margin: 10 }}>Footer</Text>
   </SafeAreaView>
 );
 
